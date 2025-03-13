2098a2f5da79ec8747b74f9bb97f4195a7fb1051
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-03-13 20:12:01 +0530

    Revert "splash and onboarding screen"
    This reverts commit 4814e5cd5a67665678a7a5f75fdfda77dfdab386.

--- lib/presentation/blocs/onboarding/onboarding_event.dart
+++
@@ -1,8 +0,0 @@
-abstract class OnboardingEvent {}
-
-class OnboardingPageChangedEvent extends OnboardingEvent {
-  final int pageIndex;
-  OnboardingPageChangedEvent(this.pageIndex);
-}
-
-class OnboardingCompleteEvent extends OnboardingEvent {}
